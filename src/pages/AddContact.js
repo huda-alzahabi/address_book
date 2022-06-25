e36843cd8b95dd3b9b5bedcab23147c371e99c77
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-25 19:01:08 +0300

    Implemented add contact functionality

--- src/pages/AddContact.js
+++ src/pages/AddContact.js
@@ -1,7 +1,199 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 const AddContact = () => {
-  return <div>AddContact</div>;
+  const nav = useNavigate();
+  // States for adding a new contact
+  const [full_name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setNumber] = useState("");
+  const [relationship_status, setRelationship] = useState("");
+  const [location, setLocation] = useState([]);
+
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the phone number change
+  const handleNumber = (e) => {
+    setNumber(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the relationship status change
+  const handleRelationship = (e) => {
+    setRelationship(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the location change
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handling the form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      full_name === "" ||
+      email === "" ||
+      phone_number === "" ||
+      relationship_status === "" ||
+      location === []
+    ) {
+      setError(true);
+    } else {
+      addContact();
+      setName("");
+      setEmail("");
+      setNumber("");
+      setRelationship("");
+      setLocation([]);
+      setSubmitted(true);
+      setError(false);
+    }
+  };
+
+  // Showing success message
+  const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      ></div>
+    );
+  };
+
+  // Showing error message if error is true
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+  const backtoContacts = () => {
+    nav("/Contacts");
+  };
+
+  //Sending the user to the users table in the db
+  const addContact = async () => {
+    let _data = {
+      full_name,
+      email,
+      phone_number,
+      relationship_status,
+      location,
+      user: localStorage.getItem("user_id"),
+    };
+
+    await fetch("http://127.0.0.1:3030/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(_data),
+    });
+    nav("/Contacts");
+  };
+
+  return (
+    <div className="background">
+      <div className="container">
+        <div>
+          <h1>New Contact</h1>
+        </div>
+
+        {/* Calling to the methods */}
+        <div className="messages">
+          {errorMessage()}
+          {successMessage()}
+        </div>
+
+        <form>
+          {/* Labels and inputs for form data */}
+          <label className="label">Name</label>
+          <input
+            onChange={handleName}
+            className="input"
+            value={full_name}
+            placeholder={"Full Name"}
+            type="text"
+          />
+
+          <label className="label">Email</label>
+          <input
+            onChange={handleEmail}
+            className="input"
+            value={email}
+            placeholder={"Email"}
+            type="email"
+          />
+
+          <label className="label">Phone Number</label>
+          <input
+            onChange={handleNumber}
+            className="input"
+            value={phone_number}
+            placeholder={"Phone Number"}
+            type="text"
+          />
+          <label className="label">Relationship Status</label>
+          <input
+            onChange={handleRelationship}
+            className="input"
+            value={relationship_status}
+            placeholder={"Relationship Status"}
+            type="text"
+          />
+          <label className="label">Location</label>
+          <input
+            onChange={handleLocation}
+            className="input"
+            value={location}
+            placeholder={"Location"}
+            type="text"
+          />
+          <div className="centerbtn">
+            <Button
+              color={"#C29876"}
+              text={"Save Contact"}
+              onClick={handleSubmit}
+            />
+          </div>
+          <h2
+            role="button"
+            style={{ color: "#C29876", cursor: "pointer" }}
+            onClick={() => backtoContacts()}
+          >
+            Back
+          </h2>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddContact;
