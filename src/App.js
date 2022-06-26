520e5c649c19cd7991bc8b0696b578ca55c9b646
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-26 17:25:43 +0300

    Created Map Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signup from "./components/Signup";
 import ViewContacts from "./pages/ViewContacts";
 import AddContact from "./pages/AddContact";
 import EditContact from "./pages/EditContact";
+import Map from "./components/Map";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/Contacts" element={<ViewContacts />}></Route>
           <Route path="/AddContact" element={<AddContact />}></Route>
           <Route path="/EditContact" element={<EditContact />}></Route>
+          <Route path="/Location" element={<Map />}></Route>
         </Routes>
       </>
     </BrowserRouter>
